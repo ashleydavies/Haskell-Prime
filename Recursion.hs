80f864794da0690ab3ee2408775e42187e1f6fe2
Brian Mitchell <bmitchel@brianary.doc.ic.ac.uk>
2015-10-09 11:43:06 +0100

    Initial commit

---
+++ Recursion.hs
@@ -0,0 +1,36 @@
+module Recursion where
+
+-- Precondition on all integers: they're all non-negative.
+
+isPrime :: Int -> Bool
+isPrime = error "TODO: implement isPrime"
+
+
+nextPrime :: Int -> Int
+nextPrime = error "TODO: implement nextPrime"
+
+
+modPow :: Int -> Int -> Int -> Int
+-- Pre: 1 <= m <= sqrt(maxint)
+modPow = error "TODO: implement modPow"
+
+
+isCarmichael :: Int -> Bool
+isCarmichael = error "TODO: implement isCarmichael"
+
+
+primeFactors :: Int -> [ Int ]
+-- Pre: x >= 1
+primeFactors = error "TODO: implement primeFactors"
+
+
+sumDigits :: Int -> Int
+sumDigits = error "TODO: implement sumDigits"
+
+
+sumAllDigits :: [ Int ] -> Int
+sumAllDigits = error "TODO: implement sumAllDigits"
+
+
+nextSmithNumber :: Int -> Int
+nextSmithNumber = error "TODO: implement nextSmithNumber"
